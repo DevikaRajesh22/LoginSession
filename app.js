8042d4f7cd9ed182ce816eb3abf388a123f15dd4
DevikaRajesh22 <devikaraj699@gmail.com>
2023-08-15 23:01:57 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// let promise = new Promise((res, rej)=>{
+//     // res('resolved');
+//     rej('reje')
+// })
+// promise.then((value)=>{
+//     console.log(value);
+// }).catch((err)=>{
+//     // console.log(err);
+// })
